8da0a90ae0f0a1b671dfb50a016a93c391a5b91d
dipak1920basnet <dipak1920basnet@gmail.com>
2026-06-17 04:05:46 +0545

    write function to get the derivative of a parameter

---
+++ src/derivate_parameter.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def Derivate_parameter():
+    ...
+
+def derivate_Z(W,dZ,dg_Z):
+    """
+    formula: 
+    dZl = (W*dZ)*dg_Z
+    """
+    return (np.transpose(W)*dZ)*dg_Z
+
+
+def derivate_W(dZl,A,m):
+    """
+    dW = (1/m)*dZl*A
+    """
+    return (dZl*np.transpose(A))/m
+
+def derivate_b(dZl,m):
+    """
+    db = (1/m)*np.sum(dZl)
+    """
+    return np.sum(dZl)/m
+
+if __name__ == "__main__":
+    Derivate_parameter()
