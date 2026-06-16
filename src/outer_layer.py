b22e6c51b80675b4d3ab0590ccc74c5a61f000db
dipak1920basnet <dipak1920basnet@gmail.com>
2026-06-17 04:06:42 +0545

    write function to calculate derivate of outer layer as per the functions

---
+++ src/outer_layer.py
@@ -0,0 +1,32 @@
+import numpy as np 
+from derivate_activation import function_derivate
+
+def Outer_layer():
+    dZ_last = {
+        "Sigmoid":Sigmoid,
+        "Softmax":Sigmoid,
+        "Linear":Sigmoid,
+        "Tanh":Tanh,
+        "Relu":ReLu,
+        "Leakyrelu":Leaky_Relu
+    }
+
+    return dZ_last
+
+def Sigmoid(A,Y):
+    return A-Y
+
+def Tanh(A,Y):
+    return Sigmoid(A,Y)*(1-np.square(A))
+
+def ReLu(A,Y,Z):
+    dZ= np.where(Z>=0,1.0,0.0)
+    return (A-Y)*dZ
+
+def Leaky_Relu(A,Y,Z):
+    d_g_Z = function_derivate()["Leakyrelu"](Z)
+    return (A-Y)*d_g_Z
+
+
+if __name__ == "__main__":
+    Outer_layer()
